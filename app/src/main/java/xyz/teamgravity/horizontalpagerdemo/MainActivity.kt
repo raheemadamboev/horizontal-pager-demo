b70c1628136ed647d834fdde51cc65177d9ab2b3
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-18 14:32:30 +0500

    Implemented HorizontalPager;

--- app/src/main/java/xyz/teamgravity/horizontalpagerdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/horizontalpagerdemo/MainActivity.kt
@@ -3,10 +3,33 @@ package xyz.teamgravity.horizontalpagerdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import xyz.teamgravity.horizontalpagerdemo.ui.Image
 import xyz.teamgravity.horizontalpagerdemo.ui.theme.HorizontalPagerDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +42,62 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val pagerState = rememberPagerState()
+                    val scope = rememberCoroutineScope()
 
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        HorizontalPager(
+                            pageCount = Image.VALUE.size,
+                            state = pagerState,
+                            key = { Image.VALUE[it] },
+                            pageSize = PageSize.Fill
+                        ) { index ->
+                            Image(
+                                painter = painterResource(id = Image.VALUE[index]),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                        Box(
+                            modifier = Modifier
+                                .offset(y = (-16).dp)
+                                .fillMaxWidth(0.5F)
+                                .clip(RoundedCornerShape(100.dp))
+                                .background(MaterialTheme.colorScheme.background)
+                                .padding(8.dp)
+                                .align(Alignment.BottomCenter)
+                        ) {
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                                    }
+                                },
+                                modifier = Modifier.align(Alignment.CenterStart)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowLeft,
+                                    contentDescription = null
+                                )
+                            }
+                            IconButton(
+                                onClick = {
+                                    scope.launch {
+                                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                                    }
+                                },
+                                modifier = Modifier.align(Alignment.CenterEnd)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowRight,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/xyz/teamgravity/horizontalpagerdemo/ui/Image.kt
@@ -0,0 +1,12 @@
+package xyz.teamgravity.horizontalpagerdemo.ui
+
+import xyz.teamgravity.horizontalpagerdemo.R
+
+object Image {
+    val VALUE = listOf(
+        R.drawable.image_1,
+        R.drawable.image_2,
+        R.drawable.image_3,
+        R.drawable.image_4
+    )
+}
