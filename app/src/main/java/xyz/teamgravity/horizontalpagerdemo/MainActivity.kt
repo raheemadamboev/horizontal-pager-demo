fe743f3f4c64f0530a53a73206a7ecc1ec0c3115
raheemadamboev <vividricemaryandmax@gmail.com>
2023-05-18 13:56:14 +0500

    Initial;

---
+++ app/src/main/java/xyz/teamgravity/horizontalpagerdemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package xyz.teamgravity.horizontalpagerdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.horizontalpagerdemo.ui.theme.HorizontalPagerDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HorizontalPagerDemoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
